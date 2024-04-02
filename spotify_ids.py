96324dced6402f467f39c70dd9fed97c49a259e4
vaslyb <lyberatosbill@hotmail.com>
2024-04-02 15:19:59 +0300

    add email notification

--- spotify_ids.py
+++ spotify_ids.py
@@ -9,23 +9,34 @@ import random
 import requests
 import sys
 import json
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib
 
 # Load credentials from config file
 with open('config.json') as f:
     config = json.load(f)
+    
+def send_email(subject, body):
+    # Email configuration
+    sender_email = 'vlyberatos@gmail.com'  # Replace with your email address
+    receiver_email = 'lyberatosbill@gmail.com'  # Replace with the recipient's email address
+    password = config['password']  # Replace with your email password
 
-# Extract client_id and client_secret
-client_id = config['client_id']
-client_secret = config['client_secret']
-client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager, requests_timeout = None) 
+    # Create the MIME object
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['To'] = receiver_email
+    message['Subject'] = subject
 
-# Spotify API URIs
-SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
-SPOTIFY_API_BASE_URL = "https://api.spotify.com"
-API_VERSION = "v1"
-SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
-genre_file = 'genres.json'
+    # Attach the body of the email
+    message.attach(MIMEText(body, 'plain'))
+
+    # Connect to the SMTP server (for Gmail, use 'smtp.gmail.com')
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()  # Start TLS encryption
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
 
 def get_token():
     client_token = base64.b64encode("{}:{}".format(client_id, client_secret).encode('UTF-8')).decode('ascii')
@@ -34,19 +45,6 @@ def get_token():
     token_request = requests.post(SPOTIFY_TOKEN_URL, data=payload, headers=headers)
     access_token = json.loads(token_request.text)["access_token"]
     return access_token
-
-TracksIds = []
-TracksIndexes = []
-TracksArtist = []
-TracksArtistId = []
-TracksPopularity = []
-TracksAlbum = []
-TracksAlbumId = []
-TracksReleaseDate = []
-TracksDuration = []
-TracksName = []
-TracksGenre = []
-TracksPreview = []  
 
 def request_valid_song(access_token, genre=None):
 
@@ -114,50 +112,82 @@ def request_valid_song(access_token, genre=None):
       
     return song,artist,artist_id,id,preview_url,album,album_id,release_date,duration,popularity
 
-p = 0
+if __name__ == "__main__":
+    try:
 
-# Get a Spotify API token
-access_token = get_token()  
+        # Extract client_id and client_secret
+        client_id = config['client_id']
+        client_secret = config['client_secret']
+        client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+        sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager, requests_timeout = None) 
 
-# Open genres file
-try:
-    with open(genre_file, 'r') as infile:
-        valid_genres = json.load(infile)
-except FileNotFoundError:
-    print("Couldn't find genres file!")
-    sys.exit(1)
+        # Spotify API URIs
+        SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+        SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+        API_VERSION = "v1"
+        SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
+        genre_file = 'genres.json'
 
-consecutive_none_count = 0
+        TracksIds = []
+        TracksIndexes = []
+        TracksArtist = []
+        TracksArtistId = []
+        TracksPopularity = []
+        TracksAlbum = []
+        TracksAlbumId = []
+        TracksReleaseDate = []
+        TracksDuration = []
+        TracksName = []
+        TracksGenre = []
+        TracksPreview = []  
 
-while consecutive_none_count < len(valid_genres):
-    consecutive_none_count = 0
-    for selected_genre in valid_genres:
-        song,artist,artist_id,id,preview_url,album,album_id,release_date,duration,popularity = request_valid_song(access_token, genre=selected_genre)
-        if id is not None:
-            TracksIds.append(id)
-            TracksName.append(song)
-            TracksArtist.append(artist)
-            TracksGenre.append(selected_genre)
-            TracksPreview.append(preview_url)
-            TracksArtistId.append(artist_id)
-            TracksAlbum.append(album)
-            TracksAlbumId.append(album_id)
-            TracksReleaseDate.append(release_date)
-            TracksDuration.append(duration)
-            TracksPopularity.append(popularity)
-            TracksIndexes.append(p)  
-            p += 1
-        else:
-            valid_genres.remove(selected_genre)  # Remove genre from list
-            consecutive_none_count += 1  # Increase the count of consecutive None results
+        p = 0
 
-with open('tracks-ids.pkl', 'wb') as output_file:
-    pickle.dump(TracksIds, output_file)
-with open('tracks-indexes.pkl', 'wb') as output_file:
-    pickle.dump(TracksIndexes, output_file)
+        # Get a Spotify API token
+        access_token = get_token()  
 
-zippedList =  list(zip(TracksIndexes, TracksName, TracksIds, TracksArtist, TracksArtistId, TracksGenre, TracksPreview, TracksAlbum, TracksAlbumId, TracksReleaseDate, TracksDuration, TracksPopularity))
+        # Open genres file
+        try:
+            with open(genre_file, 'r') as infile:
+                valid_genres = json.load(infile)
+        except FileNotFoundError:
+            print("Couldn't find genres file!")
+            sys.exit(1)
 
-df = pd.DataFrame(zippedList, columns = ['Index', 'Name' , 'ID', 'Artist', "Artist's ID", 'Genre',  'Preview', 'Album', "Album's ID", "Release Date", "Duration", "Popularity"], index=TracksIndexes) 
-  
-df.to_csv('./tracks.csv',index=False)
+        consecutive_none_count = 0
+
+        while consecutive_none_count < len(valid_genres):
+            consecutive_none_count = 0
+            for selected_genre in valid_genres:
+                song,artist,artist_id,id,preview_url,album,album_id,release_date,duration,popularity = request_valid_song(access_token, genre=selected_genre)
+                if id is not None:
+                    TracksIds.append(id)
+                    TracksName.append(song)
+                    TracksArtist.append(artist)
+                    TracksGenre.append(selected_genre)
+                    TracksPreview.append(preview_url)
+                    TracksArtistId.append(artist_id)
+                    TracksAlbum.append(album)
+                    TracksAlbumId.append(album_id)
+                    TracksReleaseDate.append(release_date)
+                    TracksDuration.append(duration)
+                    TracksPopularity.append(popularity)
+                    TracksIndexes.append(p)  
+                    p += 1
+                else:
+                    valid_genres.remove(selected_genre)  # Remove genre from list
+                    consecutive_none_count += 1  # Increase the count of consecutive None results
+
+        with open('tracks-ids.pkl', 'wb') as output_file:
+            pickle.dump(TracksIds, output_file)
+        with open('tracks-indexes.pkl', 'wb') as output_file:
+            pickle.dump(TracksIndexes, output_file)
+
+        zippedList =  list(zip(TracksIndexes, TracksName, TracksIds, TracksArtist, TracksArtistId, TracksGenre, TracksPreview, TracksAlbum, TracksAlbumId, TracksReleaseDate, TracksDuration, TracksPopularity))
+
+        df = pd.DataFrame(zippedList, columns = ['Index', 'Name' , 'ID', 'Artist', "Artist's ID", 'Genre',  'Preview', 'Album', "Album's ID", "Release Date", "Duration", "Popularity"], index=TracksIndexes) 
+        
+        df.to_csv('./tracks.csv',index=False)
+        send_email("Script Completed "+ args.save_path, "Your Python script has finished successfully.")
+    except Exception as e:
+        send_email("Script Error", f"An error occurred in your Python script:\n\n{str(e)}")
